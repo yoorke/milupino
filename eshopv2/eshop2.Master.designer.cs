ac999b08d9c5507a0c41dfc498c5c419486f7be4
yoorke <dejanjurisic@yahoo.com>
2016-07-21 11:08:53 +0200

    compare

--- eshopv2/eshop2.Master.designer.cs
+++ eshopv2/eshop2.Master.designer.cs
@@ -121,6 +121,15 @@ namespace eshopv2 {
         protected global::eshopv2.user_controls.MainMenuVertical mainMenu2;
         
         /// <summary>
+        /// btnCompare control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnCompare;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
